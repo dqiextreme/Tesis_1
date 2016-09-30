a35afddf44d2a0217d0444ec93472d38c330d37f
jechandi <joseechandi@sodicaonline.com>
2016-09-30 16:53:30 -0400

    Tesis_1

---
+++ Tesis_1/MDIParent1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Tesis_1
+{
+    partial class MDIParent1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.juegosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.memoramaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.juegosToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(632, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "MenuStrip";
+            // 
+            // juegosToolStripMenuItem
+            // 
+            this.juegosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.memoramaToolStripMenuItem,
+            this.removToolStripMenuItem});
+            this.juegosToolStripMenuItem.Name = "juegosToolStripMenuItem";
+            this.juegosToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.juegosToolStripMenuItem.Text = "Juegos";
+            // 
+            // memoramaToolStripMenuItem
+            // 
+            this.memoramaToolStripMenuItem.Name = "memoramaToolStripMenuItem";
+            this.memoramaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.memoramaToolStripMenuItem.Text = "Memorama";
+            this.memoramaToolStripMenuItem.Click += new System.EventHandler(this.memoramaToolStripMenuItem_Click);
+            // 
+            // removToolStripMenuItem
+            // 
+            this.removToolStripMenuItem.Name = "removToolStripMenuItem";
+            this.removToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.removToolStripMenuItem.Text = "remov";
+            this.removToolStripMenuItem.Click += new System.EventHandler(this.removToolStripMenuItem_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 431);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(632, 22);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "StatusStrip";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusLabel.Text = "Estado";
+            // 
+            // MDIParent1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 453);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MDIParent1";
+            this.Text = "MDIParent1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.ToolStripMenuItem juegosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem memoramaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removToolStripMenuItem;
+    }
+}
+
+
+
---
+++ Tesis_1/MDIParent1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesis_1.Control;
+
+namespace Tesis_1
+{
+    public partial class MDIParent1 : Form
+    {
+        public static int Grid_Size = 4;
+        
+        
+        private int childFormNumber = 0;
+
+        public MDIParent1()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowNewForm(object sender, EventArgs e)
+        {
+            Form childForm = new Form();
+            childForm.MdiParent = this;
+            childForm.Text = "Ventana " + childFormNumber++;
+            childForm.Show();
+        }
+
+        private void memoramaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Form frmM = this;
+            //mctrl.Location = new Point(0, 0);
+            //mctrl.Name = "mem_Ctrl1";
+            //mctrl.Size = new Size(450, 300);
+            //Controls.Add(mctrl);
+            t1();
+        }
+        Memorama2 mctrl;// = new Memorama2(Grid_Size);
+        public void t1()
+        {
+            Controls.RemoveByKey("Memorama");
+            mctrl = new Memorama2(Grid_Size);
+            Form frmM = this;
+            mctrl.Dock = DockStyle.Fill;
+            mctrl.Name = "Memorama";
+            Controls.Add(mctrl);
+        }
+
+        private void removToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Controls.RemoveByKey("Memorama");
+            mctrl.Dispose();
+        }
+
+        
+
+        
+
+
+        
+    }
+}
